be3970d819ca7a09d4297b0ba7e64661cc80aa1c
Ródney Antonius <rodney.agro@gmail.com>
2016-12-19 21:53:02 -0200

    initial footer buttons implemented

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -28,6 +28,33 @@ export class HomePage {
       stops: 0,
       time_left: "2h33m",
       to: "AMS"
+    },
+    {
+      arrival: "01:24",
+      company_icon_name: "ios-american-football",
+      departure: "20:01",
+      from: "CMX",
+      stops: 0,
+      time_left: "1h33m",
+      to: "LPA"
+    },
+    {
+      arrival: "05:55",
+      company_icon_name: "ios-at",
+      departure: "14:00",
+      from: "PKQ",
+      stops: 0,
+      time_left: "2h33m",
+      to: "BRA"
+    },
+    {
+      arrival: "02:54",
+      company_icon_name: "ios-bonfire",
+      departure: "07:44",
+      from: "VNL",
+      stops: 0,
+      time_left: "1h01m",
+      to: "CAN"
     }
   ];
 
